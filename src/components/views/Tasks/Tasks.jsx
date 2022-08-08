2ba6fb131ad383469b883d2050ad50d1dee9dc53
María Imbert <imbert.maria@hotmail.com>
2022-08-08 19:46:59 -0300

    task component - styles

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -1,83 +1,226 @@
+import { useState, useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import "./Tasks.styles.css";
+import { getTasks, deleteTask, editTaskStatus } from "../../../store/actions/tasksActions";
+import {
+    Radio,
+    RadioGroup,
+    FormControl,
+    FormControlLabel,
+} from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import debounce from "lodash.debounce";
+import { useResize } from "../../hooks/useResize";
+import { Header } from "../../Header/Header";
+import { Card } from "../../Card/Card";
+import { TaskForm } from "../../TaskForm/TaskForm";
 
-import "./Tasks.styles.css"
-import {useResize} from "../../hooks/useResize"
-import { Header } from "../../Header/Header"
-import {cardsData} from "./data"
-import {Card} from "../../Card/Card"
-import {TaskForm} from "../../TaskForm/TaskForm"
+// const { REACT_APP_API_ENDPOINT: API_ENDPOINT } = process.env;
 
 export const Tasks = () => {
-
+    const [list, setList] = useState(null);
+    const [renderList, setRenderList] = useState(null);
+    const [tasksfromWho, setTasksfromWho] = useState("ALL");
+    const [search, setSearch] = useState("");
+    // const [loading, setLoading] = useState(false);
     const { isPhone } = useResize();
+    const dispatch = useDispatch();
 
-    const limitString = str => {
-        if(str.length > 370)
-            return {string: str.slice(0, 367).concat("..."), addButton: true}
-        return { string: str, addButton: false}
-    }
+    //al hacer un get no necesitamos aclarar el metodo GET
+
+    //USE EFFECT SIN REDUX
+    // useEffect(() => {
+    //     setLoading(true);
+    //     fetch(`${API_ENDPOINT}task${tasksfromWho === "ME" ? "/me" : ""}`, {
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Authorization: "Bearer " + localStorage.getItem("token"),
+    //         },
+    //     })
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setList(data.result);
+    //             setRenderList(data.result);
+    //             setLoading(false);
+    //             console.log(data);
+    //         });
+    // }, [tasksfromWho]);
+
+    //USE EFFECT CON REDUX
+    useEffect(() => {
+        dispatch(getTasks(tasksfromWho === "ME" ? "/me" : ""));
+    }, [tasksfromWho, dispatch]);
+
+    const { loading, error, tasks } = useSelector((state) => {
+        return state.tasksReducer;
+    });
+
+
+    useEffect(() => {
+        if (tasks?.length) {
+            setList(tasks);
+            setRenderList(tasks);
+        }
+    }, [tasks]);
+
+    useEffect(() => {
+        if (search) {
+            setRenderList(list.filter((data) => data.title.startsWith(search)));
+        } else setRenderList(list);
+    }, [search]);
 
     const renderAllCards = () => {
-        return cardsData.map( data => <Card key={data.id} data={data} />)
-    }
+        return renderList?.map((data) => (
+            <Card
+                key={data.id}
+                data={data}
+                deleteCard={handleDelete}
+                editCardStatus={handleEditCardStatus}
+            />
+        ));
+    };
+
+    //Devuelve cards de status nuevo
+    const renderColumnCards = (text) => {
+        return renderList
+            ?.filter((data) => data.status === text)
+            .map((data) => (
+                <Card
+                    key={data._id}
+                    data={data}
+                    deleteCard={handleDelete}
+                    editCardStatus={handleEditCardStatus}
+                />
+            ));
+    };
+
+    const handleChangeImportance = (event) => {
+        if (event.currentTarget.value === "ALL") setRenderList(list);
+        else
+            setRenderList(
+                list.filter(
+                    (data) => data.importance === event.currentTarget.value
+                )
+            );
+    };
+
+    // const handleSearch = event => setSearch(event.currentTarget.value)
+
+    const handleSearch = debounce((event) => {
+        setSearch(event?.target?.value);
+    }, 1000);
+
+    const handleDelete = (id) => dispatch(deleteTask(id));
+
+    const handleEditCardStatus = data => dispatch(editTaskStatus(data))
+
+    if (error) return <div>hay un error</div>;
 
     return (
         <>
-            <Header/>
+            <Header />
             <main id="tasks">
-                <TaskForm/>
+                <TaskForm />
                 <section className="wrapper_list">
                     <div className="list_header">
                         <h2>Mis tareas</h2>
                     </div>
-                    { isPhone ? (
-                        <div className="list phone">
-                            {renderAllCards()}
+                    <div className="filters">
+                        <FormControl>
+                            <RadioGroup
+                                row
+                                aria-labelledby="demo-row-radio-buttons-group-label"
+                                onChange={(event) => {
+                                    setTasksfromWho(event.currentTarget.value);
+                                }}
+                            >
+                                <FormControlLabel
+                                    value="ALL"
+                                    control={<Radio />}
+                                    label="Todas"
+                                />
+                                <FormControlLabel
+                                    value="ME"
+                                    control={<Radio />}
+                                    label="Mis tareas"
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                        <div className="search">
+                            <input
+                                type="text"
+                                placeholder="Buscar por título..."
+                                // value={search}
+                                onChange={handleSearch}
+                            />
                         </div>
+                        <select
+                            name="importance"
+                            onChange={handleChangeImportance}
+                        >
+                            <option value="">Seleccionar una prioridad</option>
+                            <option value="ALL">Todas</option>
+                            <option value="LOW">Baja</option>
+                            <option value="MEDIUM">Media</option>
+                            <option value="HIGH">Alta</option>
+                        </select>
+                    </div>
+
+                    {isPhone ? (
+                        !list?.length ? (
+                            <div>No hay tareas creadas</div>
+                        ) : loading ? (
+                            <>
+                                <Skeleton heigh={90} baseColor="lightgrey" />
+                                <Skeleton heigh={90} baseColor="lightgrey" />
+                                <Skeleton heigh={90} baseColor="lightgrey" />
+                            </>
                         ) : (
+                            <div className="list phone">{renderAllCards()}</div>
+                        )
+                    ) : (
                         <div className="list_group">
-                            <div className="list">
-                                <h4>Nuevas</h4>
-                                <div className="card">
-                                    <div className="close">x</div>
-                                    <h3>Tarea 1</h3>
-                                    <h6>24/1/2022 16:40 hs.</h6>
-                                    <h5>María Imbert</h5>
-                                    <button type="button">Nueva</button>
-                                    <button type="button">Alta</button>
-                                    <p>{
-                                        limitString(`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim delectus laborum sapiente porro quasi sunt dolorem ipsam est voluptatum fuga assumenda laboriosam beatae blanditiis eligendi, sequi facilis architecto minima. Ea! Lorem, ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, sint? Rem sit nisi minus et eligendi doloribus quae, ipsa cumque quod tempore molestias vitae rerum qui! Sunt animi voluptatem voluptatibus?loremrchitecto minima. Ea! Lorem, ipsum dolor sit amet consectetur adipisicing. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, sint? Rem sit nisi minus et eligendi doloribus quae, ipsa cumque quod tempore molestias vitae r`).string
-                                        }
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="list">
-                                <h4>En proceso</h4>
-                                <div className="card">
-                                    <div className="close">x</div>
-                                    <h3>Tarea 1</h3>
-                                    <h6>24/1/2022 16:40 hs.</h6>
-                                    <h5>María Imbert</h5>
-                                    <button type="button">Nueva</button>
-                                    <button type="button">Alta</button>
-                                    <p>Descipción fake</p>
-                                </div>
-                            </div>
-                            <div className="list">
-                                <h4>Finalizadas</h4>
-                                <div className="card">
-                                    <div className="close">x</div>
-                                    <h3>Tarea 1</h3>
-                                    <h6>24/1/2022 16:40 hs.</h6>
-                                    <h5>María Imbert</h5>
-                                    <button type="button">Nueva</button>
-                                    <button type="button">Alta</button>
-                                    <p>Descipción fake</p>
-                                </div>
-                            </div>
+                            {!list?.length ? (
+                                <div>No hay tareas creadas</div>
+                            ) : loading ? (
+                                <>
+                                    <Skeleton
+                                        heigh={150}
+                                        width={300}
+                                        baseColor="lightgrey"
+                                    />
+                                    <Skeleton
+                                        heigh={150}
+                                        width={300}
+                                        baseColor="lightgrey"
+                                    />
+                                    <Skeleton
+                                        heigh={150}
+                                        width={300}
+                                        baseColor="lightgrey"
+                                    />
+                                </>
+                            ) : (
+                                <>
+                                    <div className="list">
+                                        <h4>Nuevas</h4>
+                                        {renderColumnCards("NEW")}
+                                    </div>
+                                    <div className="list">
+                                        <h4>En proceso</h4>
+                                        {renderColumnCards("IN PROGRESS")}
+                                    </div>
+                                    <div className="list">
+                                        <h4>Finalizadas</h4>
+                                        {renderColumnCards("FINISHED")}
+                                    </div>
+                                </>
+                            )}
                         </div>
                     )}
                 </section>
             </main>
         </>
-    )
-}
+    );
+};
