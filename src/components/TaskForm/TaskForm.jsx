5b45484d38183fff439dcf285652715583c34bef
María Imbert <imbert.maria@hotmail.com>
2022-08-08 19:48:16 -0300

    Task Form component - styles

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,35 +1,51 @@
-import "./TaskForm.styles.css"
-import * as Yup from 'yup'
+import "./TaskForm.styles.css";
+import * as Yup from 'yup';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import {useFormik} from 'formik';
 
+const {REACT_APP_API_ENDPOINT: API_ENDPOINT} = process.env
 
 export const TaskForm = () => {
 
-        const initialValues = {
-            title: "",
-            status: "",
-            priority: "",
-            description: "",
+    const initialValues = {
+        title: "",
+        status: "",
+        importance: "",
+        description: "",
 
-        }
+    }
     
-        const onSubmit = (e) => {
-            alert()
-        }
+    const onSubmit = () => {
+        fetch(`${API_ENDPOINT}task`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("token"),
+            },
+            body: JSON.stringify ({ task: values}),
+        })
+                .then(response => response.json())
+                .then(data=> {
+                    resetForm()
+                    toast("tu tarea se creo")
+                })
+    }
 
         const required = "* Campo obligatorio"
 
-        const validationsSchema = Yup.object().shape({
+        const validationSchema = Yup.object().shape({
             title: Yup.string()
                 .min(6, "La cantidad minima de caracteres es 6")
                 .required(required),
             status: Yup.string().required(required),
-            priority: Yup.string().required(required)
+            description: Yup.string().required(required),
+            importance: Yup.string().required(required)
         }) 
         const formik = useFormik ({initialValues, validationSchema, onSubmit})
 
-        const { handleSubmit, handleChange, errors } = formik
+        const { handleSubmit, handleChange, handleBlur, errors, touched, values, resetForm } = formik
     
     return (
         <section className="task-form">
@@ -38,33 +54,63 @@ export const TaskForm = () => {
             <form onSubmit={handleSubmit}>
                 <div>
                     <div>
-                        <input name="title" onChange={handleChange} placeholder="Título"></input>
+                        <input 
+                            name="title" 
+                            onChange={handleChange} 
+                            onBlur={handleBlur} 
+                            placeholder="Título"
+                            className={errors.title && touched.title? "error" : ""}   
+                            value={values.title}
+                            >
+                        </input>
                     </div>
-                    {errors.title && <span>errors.title</span>}
+                    {errors.title && touched.title && <span className="error-message">{errors.title}</span>}
                     <div>
-                        <select name="status" onChange={handleChange}>
+                        <select 
+                        name="status" 
+                        onChange={handleChange}         
+                        onBlur={handleBlur}                     
+                        className={errors.status && touched.status? "error" : ""}
+                        value={values.status}
+                        > 
+                        
                             <option value="">Seleccionar un estado</option>
-                            <option value="new">Nueva</option>
-                            <option value="inProcess">En Proceso</option>
-                            <option value="finished">Terminada</option>
+                            <option value="NEW">Nueva</option>
+                            <option value="IN PROGRESS">En Proceso</option>
+                            <option value="FINISHED">Terminada</option>
                         </select>
                     </div>
-                        {errors.status && <span>errors.status</span>}
+                        {errors.status && touched.status && <span>{errors.status}</span>}
                     <div>
-                        <select name="priority" onChange={handleChange}>
+                        <select 
+                            name="importance" 
+                            onChange={handleChange} 
+                            onBlur={handleBlur}                             
+                            className={errors.importance && touched.importance? "error" : ""}
+                            value={values.importance}
+                        >        
                             <option value="">Seleccionar una prioridad</option>
-                            <option value="low">Baja</option>
-                            <option value="medium">Media</option>
-                            <option value="high">Alta</option>
+                            <option value="LOW">Baja</option>
+                            <option value="MEDIUM">Media</option>
+                            <option value="HIGH">Alta</option>
                         </select>
                     </div>
-                        {errors.priority && <span>errors.priority</span>}
+                        {errors.importance && touched.importance && <span className="error-message">{errors.importance}</span>}
                 </div>
                 <div>
-                    <textarea name="description" onChange={handleChange} placeholder="Descripción"></textarea>
+                    <textarea 
+                        name="description" 
+                        onChange={handleChange} 
+                        placeholder="Descripción"
+                        onBlur={handleBlur}
+                        className={errors.description && touched.description? "error" : ""}
+                        value={values.description}
+                    />
+                    {errors.description && touched.description && <span className="error-message">{errors.description}</span>}
                 </div>
                 <button type="submit">Crear</button>
             </form>
+            <ToastContainer />
 
         </section>
     )
