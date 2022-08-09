d0ec588eeb04607656be0d50ba3b65e2dba555ac
María Imbert <imbert.maria@hotmail.com>
2022-08-09 18:17:12 -0300

    Header tasks number

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import "./Header.styles.css";
 import {useNavigate} from "react-router-dom"
+import { useSelector} from "react-redux";
 
 //falta cambiar el span por una imagen. <img src="/img/goscrum.png" alt="Logo"/>
 export const Header = () => {
     const navigate = useNavigate();
+
+    const { tasks } = useSelector((state) => {
+        return state.tasksReducer;
+    });
 
     const handleLogout = () => {
         localStorage.removeItem("token");
@@ -15,6 +20,7 @@ export const Header = () => {
         <header>
             <span>Go Scrum</span>
             <div className="wrapper_right_header" >
+                <div>Tareas creadas: {tasks?.length}</div>
                 <div>{localStorage.getItem("userName")}</div>
                 <div onClick={handleLogout}>x</div>
             </div>
